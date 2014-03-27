389726ce740b57222c170bd754e11e8448106bee
goutham vel <gouthamvel@gmail.com>
2014-03-27 12:31:48 +0530

    appends to PATH rather than prepend helpfull in development in other platform

--- lib/pdftk-heroku.rb
+++ lib/pdftk-heroku.rb
@@ -3,8 +3,8 @@ require "pdftk-heroku/version"
 module Pdftk
   module Heroku
 
-    ENV['PATH'] = File.expand_path("../pdftk-heroku/binaries", __FILE__) + ":#{ENV['PATH']}"
-    ENV['LD_LIBRARY_PATH'] = File.expand_path("../pdftk-heroku/binaries", __FILE__) + ":#{ENV['LD_LIBRARY_PATH']}"
+    ENV['PATH'] = "#{ENV['PATH']}:" + File.expand_path("../pdftk-heroku/binaries", __FILE__)
+    ENV['LD_LIBRARY_PATH'] = "#{ENV['LD_LIBRARY_PATH']}:" + File.expand_path("../pdftk-heroku/binaries", __FILE__)
 
   end
 end
